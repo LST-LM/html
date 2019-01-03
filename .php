2d251c0cab70cc26aaccfbef8661def9a27bc601
LST-LM <1548861368@qq.com>
2019-01-03 11:02:36 +0800

    32

--- .php
+++ .php
@@ -1,5 +1,9 @@
-<html>
-<body>
-<a href="http//github.com">hello,github</a>
-</body>
-</html>
+<?php
+header("Access-Control-Allow-Origin:http://10.30.29.73:63341");
+header("Access-Control-Allow-Credentials:true");
+header('Access-Control-Allow-Headers:Content-Type');
+
+
+$array=array("userName"=>"111111111");
+echo json_encode($array);
+?>
